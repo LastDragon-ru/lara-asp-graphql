62edc44113b313818e7b2822761d331a019c3377
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2026-02-14 11:11:58 +0400

    feat(phpunit-extensions): New extension `\LastDragon_ru\PhpUnit\Extensions\Extension` (contains all other extensions).

--- src/Package/TestCaseTest.php
+++ src/Package/TestCaseTest.php
@@ -2,34 +2,13 @@
 
 namespace LastDragon_ru\LaraASP\GraphQL\Package;
 
-use Exception;
-use LastDragon_ru\PhpUnit\Extensions\StrictScalarComparator\Extension;
+use LastDragon_ru\PhpUnit\Package\IntegrationTests;
 use PHPUnit\Framework\Attributes\CoversNothing;
-use PHPUnit\Framework\ExpectationFailedException;
-
-use function sprintf;
 
 /**
  * @internal
  */
 #[CoversNothing]
 final class TestCaseTest extends TestCase {
-    public function testIntegration(): void {
-        $exception = null;
-
-        try {
-            self::assertEquals(1, true);
-        } catch (Exception $exception) {
-            // empty
-        }
-
-        self::assertInstanceOf(
-            ExpectationFailedException::class,
-            $exception,
-            sprintf(
-                'Extension `%s` is not registered?',
-                Extension::class,
-            ),
-        );
-    }
+    use IntegrationTests;
 }
