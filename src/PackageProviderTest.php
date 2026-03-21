5271926668fcd8d1d8f45d0bf7fd3c883543faca
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2026-03-21 15:57:12 +0400

    refactor!: Array-based config not supported anymore.

--- src/PackageProviderTest.php
+++ src/PackageProviderTest.php
@@ -2,16 +2,7 @@
 
 namespace LastDragon_ru\LaraASP\GraphQL;
 
-use Illuminate\Contracts\Config\Repository;
-use LastDragon_ru\LaraASP\GraphQL\Config\Config;
 use LastDragon_ru\LaraASP\GraphQL\Package\TestCase;
-use LastDragon_ru\LaraASP\GraphQL\SearchBy\Definitions\SearchByOperatorBetweenDirective;
-use LastDragon_ru\LaraASP\GraphQL\SearchBy\Definitions\SearchByOperatorEqualDirective;
-use LastDragon_ru\LaraASP\GraphQL\SortBy\Definitions\SortByOperatorRandomDirective;
-use LastDragon_ru\LaraASP\GraphQL\SortBy\Operators as SortByOperators;
-use LastDragon_ru\LaraASP\Testing\Utils\WithTestData;
-use Nuwave\Lighthouse\Schema\Directives\RenameDirective;
-use Nuwave\Lighthouse\Validation\ValidateDirective;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 /**
@@ -19,49 +10,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
  */
 #[CoversClass(PackageProvider::class)]
 final class PackageProviderTest extends TestCase {
-    use WithTestData;
-
     public function testConfig(): void {
         self::assertConfigurationExportable(PackageConfig::class);
-    }
-
-    /**
-     * @deprecated 7.0.0 Array-base config is deprecated.
-     */
-    public function testLegacyConfig(): void {
-        // Prepare
-        $app     = $this->app();
-        $config  = $app->make(Repository::class);
-        $legacy  = (array) require self::getTestData()->path('~LegacyConfig.php');
-        $package = Package::Name;
-
-        $config->set($package, $legacy);
-
-        self::assertIsArray($config->get($package));
-
-        (new PackageProvider($app))->register();
-
-        // Test
-        $expected                                            = new Config();
-        $expected->searchBy->operators['Date']               = [
-            SearchByOperatorEqualDirective::class,
-            SearchByOperatorBetweenDirective::class,
-        ];
-        $expected->searchBy->operators['DateTime']           = ['Date'];
-        $expected->sortBy->operators[SortByOperators::Extra] = [
-            SortByOperatorRandomDirective::class,
-        ];
-        $expected->stream->search->name                      = 'custom_where';
-        $expected->stream->sort->name                        = 'custom_order';
-        $expected->stream->limit->name                       = 'custom_limit';
-        $expected->stream->limit->default                    = 5;
-        $expected->stream->limit->max                        = 10;
-        $expected->stream->offset->name                      = 'custom_offset';
-        $expected->builder->allowedDirectives                = [
-            RenameDirective::class,
-            ValidateDirective::class,
-        ];
-
-        self::assertEquals($expected, $config->get($package));
     }
 }
